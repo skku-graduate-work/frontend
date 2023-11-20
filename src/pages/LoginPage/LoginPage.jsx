02e2ed23d6eeaa64b3b579a058ea4f3df3c685ff
karuistep <karuistep@gmail.com>
2023-11-21 00:55:39 +0900

    로그인페이지 디자인 개선

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,65 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Modal from "react-modal";
-import { ToastContainer, toast } from "react-toastify";
-
-import { Login, SocialLogin } from "../../axios";
+import { Login } from "../../axios";
 import { setCookie } from "../../utils/Cookie";
-
 import SignupForm from "../Components/SignupForm";
-import FindPasswordForm from "../Components/FindPasswordForm";
-
-import randomImage1 from "../../images/Food_Login01.jpg";
-import randomImage2 from "../../images/Food_Login02.jpg";
-import randomImage3 from "../../images/Food_Login03.jpg";
-import randomImage4 from "../../images/Food_Login04.jpg";
-import randomImage5 from "../../images/Food_Login05.jpg";
-import randomImage6 from "../../images/Food_Login06.jpg";
-import randomImage7 from "../../images/Food_Login07.jpg";
-import randomImage8 from "../../images/Food_Login08.jpg";
-import randomImage9 from "../../images/Food_Login09.jpg";
-import randomImage10 from "../../images/Food_Login10.jpg";
-import randomImage11 from "../../images/Food_Login11.jpg";
-import randomImage12 from "../../images/Food_Login12.jpg";
-import randomImage13 from "../../images/Food_Login13.jpg";
-
-import LocalDiningIcon from "@mui/icons-material/LocalDining";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
 import naverIcon from "../../images/Icon_naver.png";
 import kakaoIcon from "../../images/Icon_kakao.png";
 import googleIcon from "../../images/Icon_google.jpg";
-
-import axios from "axios";
+import main_video from "../../video/main_video.mp4";
+import "./LoginPage.css";
 
 export default function LoginPage() {
-  // 이미지 파일 배열
-  const images = [
-    randomImage1,
-    randomImage2,
-    randomImage3,
-    randomImage4,
-    randomImage5,
-    randomImage6,
-    randomImage7,
-    randomImage8,
-    randomImage9,
-    randomImage10,
-    randomImage11,
-    randomImage12,
-    randomImage13,
-  ];
-
-  // 메인화면 이미지 변수
-  const [randomImage, setRandomImage] = useState("");
-
   // State 변수
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  // 회원가입 모달 창 보임 변수
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  // 비밀번호 찾기 모달 창 보임 변수
-  const [modalIsOpen2, setIsOpen2] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
+  const [modalType, setModalType] = useState("login"); // 모달 종류 추가
 
   // 모달 창 스타일
   const customStyles = {
@@ -75,7 +33,6 @@ export default function LoginPage() {
   };
 
   Modal.setAppElement("#root");
-
   const navigate = useNavigate();
 
   // 이메일 입력 핸들러
@@ -112,306 +69,185 @@ export default function LoginPage() {
       });
   };
 
-  // 회원가입 창 모달 보임함수
-  const showModal = () => {
-    setIsOpen(true);
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  // 가입하기 버튼을 클릭했을 때 모달의 내용을 변경하는 함수
+  const showSignUpModal = () => {
+    setModalType("signup");
+    setIsSignUpModalOpen(true);
   };
 
   // 회원가입 창 모달 숨김함수
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  // 비밀번호 찾기 창 모달 보임함수
-  const showModal2 = () => {
-    setIsOpen2(true);
-  };
-
-  // 비밀번호 찾기 창 모달 숨김함수
-  const closeModal2 = () => {
-    setIsOpen2(false);
+  const closeSignUpModal = () => {
+    setIsSignUpModalOpen(false);
   };
 
   // 네이버 소셜 로그인
   const handleNaverLogin = () => {
-    // SocialLogin("naver")
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
     window.location.href = "http://localhost:8080/oauth2/authorization/naver";
   };
 
   // 카카오 소셜 로그인
   const handleKakaoLogin = () => {
-    // SocialLogin("kakao")
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
     window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
   };
 
   // 구글 소셜 로그인
   const handleGoogleLogin = () => {
-    // SocialLogin("google")
-    //   .then((res) => {
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
     window.location.href = "http://localhost:8080/oauth2/authorization/google";
   };
 
-  // 랜덤한 이미지 선택 (한번만)
-  useEffect(() => {
-    setRandomImage(images[Math.floor(Math.random() * images.length)]);
-  }, []);
-
   return (
-    <div
-      className="inner"
-      style={{
-        width: "1200px",
-        height: "750px",
-        margin: "0 auto",
-        display: "flex",
-        fontFamily: "NotoSans",
-        fontWeight: "700",
-      }}
-    >
-      <div
-        className="left"
-        style={{
-          width: "50%",
-        }}
-      >
-        <img
-          src={randomImage}
-          alt="Random Image"
-          style={{ width: "100%", height: "100%" }}
-        />
+    <>
+      <video className="background-video" src={main_video} autoPlay loop muted>
+        {/* 동영상 소스 */}
+        <source src={main_video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="title">
+        <RestaurantIcon style={{ fontSize: "64px" }} /> &nbsp;사이트 제목
       </div>
-      <div
-        className="right"
+      <button className="login-button" onClick={openLoginModal}>
+        로그인
+      </button>
+      <Modal
+        isOpen={isLoginModalOpen}
+        onRequestClose={closeLoginModal}
         style={{
-          width: "50%",
-          borderRight: "2px solid #e9e9e9",
-          borderBottom: "2px solid #e9e9e9",
-        }}
-      >
-        <div
-          className="title"
-          style={{
-            height: "100px",
-            borderBottom: "2px solid #e9e9e9",
-            boxSizing: "border-box",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "40px",
-          }}
-        >
-          <LocalDiningIcon style={{ fontSize: "64px" }} /> 음식 추천 시스템
-        </div>
-        <div
-          className="loginBox"
-          style={{
+          portalClassName: "modal-portal", // 새로운 portal 클래스 추가
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+          content: {
             width: "400px",
             height: "500px",
-            margin: "75px auto",
-            textAlign: "center",
-          }}
-        >
-          <div
-            style={{
-              height: "40px",
-              fontSize: "24px",
-            }}
-          >
-            Log In
-          </div>
-          <form
-            style={{ height: "240px", marginTop: "30px" }}
-            onSubmit={handleSubmit}
-          >
-            <div
-              style={{
-                height: "30px",
-                paddingLeft: "10px",
-                textAlign: "left",
-                fontSize: "18px",
-                color: "#5e5e5e",
-              }}
-            >
-              Email
-            </div>
+            margin: "auto",
+            backgroundColor: "#FAFAFA",
+            display: "flex",
+            flexDirection: "column", // 세로 방향으로 나열
+            justifyContent: "space-between", // 컨텐츠 사이의 간격을 최대화
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)", // 그림자 추가
+            padding: "5px", // padding 제거
+            animation: "slideIn 0.5s ease-out", // 슬라이딩 애니메이션
+          },
+        }}
+      >
+        {/* 첫 번째 영역 */}
+        <div className="login-region-1">
+          <h2 className="login-region-1-title">사이트 제목</h2>
+          {/* 이메일 입력창 */}
+          <div className="email-region">
             <input
+              className="email-input"
               type="text"
               value={email}
               onChange={handleEmailChange}
-              style={{
-                width: "100%",
-                height: "40px",
-                paddingLeft: "10px",
-                boxSizing: "border-box",
-                fontSize: "16px",
-              }}
-              placeholder="이메일을 입력해주세요"
+              placeholder="이메일"
             />
-
-            <div
-              style={{
-                height: "30px",
-                marginTop: "30px",
-                paddingLeft: "10px",
-                textAlign: "left",
-                fontSize: "18px",
-                color: "#5e5e5e",
-              }}
-            >
-              Password
-            </div>
+          </div>
+          {/* 비밀번호 입력창 */}
+          <div className="password-region">
             <input
+              className="password-input"
+              style={{ color: "black" }}
               type="password"
               value={password}
               onChange={handlePasswordChange}
-              style={{
-                width: "100%",
-                height: "40px",
-                paddingLeft: "10px",
-                boxSizing: "border-box",
-                fontSize: "16px",
-              }}
-              placeholder="비밀번호를 입력해주세요"
+              placeholder="비밀번호"
             />
-
-            <button
-              type="submit"
+          </div>
+          <div className="login-button-area">
+            {/* 로그인 버튼 */}
+            <button className="login-button-2" onClick={handleSubmit}>
+              로그인
+            </button>
+          </div>
+          {/* 구분선 */}
+          <div className="line-area">
+            <hr style={{ flex: "1", borderTop: "1px solid #ccc" }} />
+            <span
               style={{
-                width: "100%",
-                height: "40px",
-                marginTop: "30px",
-                border: "0",
-                borderRadius: "4px",
-                fontSize: "18px",
-                color: "#FFFFFF",
-                backgroundColor: "#5E5E5E",
-                cursor: "pointer",
+                padding: "0 20px",
+                color: "#ccc",
+                fontWeight: "700",
+                fontFamily: "NanumSquare",
               }}
             >
-              Log In
-            </button>
-          </form>
-          <div
-            style={{
-              height: "60px",
-              marginTop: "30px",
-              textAlign: "left",
-              fontSize: "18px",
-              color: "#5e5e5e",
-            }}
-          >
-            <div>
-              계정이&nbsp;없으신가요?{" "}
-              <a
-                style={{
-                  color: "Highlight",
-                  cursor: "pointer",
-                }}
-                onClick={showModal}
-              >
-                회원가입
-              </a>
-            </div>
-            {/* <div>
-              비밀번호를 잊으셨나요?{" "}
-              <a
-                style={{
-                  color: "Highlight",
-                  cursor: "pointer",
-                }}
-                onClick={showModal2}
-              >
-                비밀번호 찾기
-              </a>
-            </div> */}
+              또는
+            </span>
+            <hr style={{ flex: "1", borderTop: "1px solid #ccc" }} />
           </div>
+          {/* 소셜 로그인 */}
+          <div className="social-area">
+            {/* 소셜 로그인 아이콘들 */}
+            <img
+              onClick={handleNaverLogin}
+              src={naverIcon}
+              alt="Naver Icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                cursor: "pointer",
+                margin: "0 5px",
+              }}
+            />
+            <img
+              onClick={handleKakaoLogin}
+              src={kakaoIcon}
+              alt="Kakao Icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                cursor: "pointer",
+                margin: "0 5px",
+              }}
+            />
+            <img
+              onClick={handleGoogleLogin}
+              src={googleIcon}
+              alt="Google Icon"
+              style={{
+                width: "30px",
+                height: "30px",
+                cursor: "pointer",
+                margin: "0 5px",
+              }}
+            />
+          </div>
+        </div>
 
-          {/* 회원가입 모달 */}
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <SignupForm closeModal={closeModal} />
-          </Modal>
-
-          {/* 비밀번호 찾기 모달 */}
-          <Modal
-            isOpen={modalIsOpen2}
-            onRequestClose={closeModal2}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <FindPasswordForm />
-          </Modal>
-
-          <div
-            style={{
-              height: "100px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <a>
-              <img
-                onClick={handleNaverLogin}
-                src={naverIcon}
-                alt="Naver Icon"
-                style={{
-                  width: "50px",
-                  height: "50px",
-                  cursor: "pointer",
-                }}
-              />
-            </a>
-            <a>
-              <img
-                onClick={handleKakaoLogin}
-                src={kakaoIcon}
-                alt="Kakao Icon"
-                style={{
-                  width: "50px",
-                  height: "50px",
-                  marginLeft: "30px",
-                  cursor: "pointer",
-                }}
-              />
-            </a>
-            <a>
-              <img
-                onClick={handleGoogleLogin}
-                src={googleIcon}
-                alt="Google Icon"
-                style={{
-                  width: "50px",
-                  height: "50px",
-                  marginLeft: "30px",
-                  cursor: "pointer",
-                }}
-              />
+        {/* 두 번째 영역 */}
+        <div className="login-region-2">
+          <div>
+            계정이 없으신가요?{" "}
+            <a
+              style={{
+                color: "Highlight",
+                cursor: "pointer",
+              }}
+              onClick={showSignUpModal} // 회원가입 모달 열기 함수 사용
+            >
+              가입하기
             </a>
           </div>
         </div>
-      </div>
-    </div>
+      </Modal>
+
+      {/* 회원가입 모달 */}
+      <Modal
+        isOpen={isSignUpModalOpen}
+        onRequestClose={closeSignUpModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <SignupForm closeModal={closeSignUpModal} />
+      </Modal>
+    </>
   );
 }
